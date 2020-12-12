2fcc4b452130c8109a94a593b7a9f247edaccbe7
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-12-12 19:11:12 +0100

    forget the comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" id="container">
+      <div className="navbar" id="navbar">
+        <div className="navlinks" id="navlinks">
+          <a href="#">HOME</a>
+          <a href="#">GALLRY</a>
+          <a href="#">ABOUT</a>
+        </div>
+        <div className="logo" id="logo">
+          <img src="assets/iconsandlogos/Koenigsegg_symbol.svg" alt="Koenigsegg Symbol"/>
+        </div>
+        <div className="navlinks2" id="navlinks2">
+          <a href="#">DESIGN</a>
+          <a href="#">PERFORMANCE</a>
+          <a href="#">CONTACT</a>
+        </div>
+      </div>
+      <div className="sideImg" id="sideImg">
+        <img src="/assets/images/koenigsegggemera-removebg-preview.png" alt="Koenigsegg Gemera"/>
+      </div>
+      <div className="content" id="content">
+        <h1>KOENIGSEGG GEMERA</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia velit ad possimus maiores nemo perspiciatis cumque beatae. Accusamus dolore, assumenda molestiae nobis voluptatum maiores deleniti quisquam sequi sunt repudiandae voluptatem!</p>
+        <button>EXPLORE</button>
+        <img src="" alt=""/>
+      </div>
     </div>
   );
 }
